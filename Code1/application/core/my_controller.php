283872ee94d0e7c4508c482766d4ea9cd5bb0cea
Leonardo <leo.abreudp@gmail.com>
2022-12-02 12:53:18 +0000

    constroller fixed

--- Code1/application/core/my_controller.php
+++ Code1/application/core/my_controller.php
@@ -5,10 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // com funções padronizadas e úteis
 abstract class my_controller extends CI_Controller {
 
-	// Variavel de strings keys necessarias para o carregamento de qualquer pagina
-	private array $data_needed_header = array('title');
-	private array $data_needed_nav = array();
-	private array $data_needed_footer = array('criador');
+	// Design Pattern Iterator
+	protected $iterator;
 
 	// Variavel que verifica se a pagina tem menu abilitado
 	private bool $load_nav = FALSE;
@@ -40,6 +38,9 @@ abstract class my_controller extends CI_Controller {
 		// Instancia as funcionalidades de ancoras
 		$this->load->helper('url');
 
+		// Cria os arrays multidimensionais
+		$this->create_data_arrays();
+
 		// Define os ficheiros de css main do site
 		$this->setMainCssFile();
 	}
@@ -48,12 +49,10 @@ abstract class my_controller extends CI_Controller {
 	//* as variaveis usadas nas views são carregadas por meio de funcionalidades
 	protected function load_views($path, $return = FALSE)
 	{
-		$this->verify_datas();
-
 		$this->load->view($this->includes_path.'/'.$this->include_header, $this->data_header);
 
 		if($this->load_nav)
-			$this->load->view($this->includes_path.'/'.$this->include_nav, $this->data_menu);
+			$this->load->view($this->includes_path.'/'.$this->include_nav, $this->data_nav);
 
 		$this->load->view($path, $this->data_body, $return);
 
@@ -63,22 +62,31 @@ abstract class my_controller extends CI_Controller {
 	//* Funcionalidade que define o titulo da pagina
 	protected function setTitle($title = 'Undefined title')
 	{
+		if(!$title || !is_string($title))
+			return;
+
 		$this->data_header['title'] = $title;
 	}
 
 	//* Adiciona ficheiros de css ao header
 	protected function setCssFiles($array)
 	{
-		foreach($array as $key => $path){
-			$this->data_header['css'][$key] = base_url($this->assets_path.'/'.$this->css_path.'/'.$path.'.css');
+		if(!$array || !is_array($array))
+			return;
+
+		foreach($array as $path){
+			$this->data_header['css'][] = base_url($this->assets_path.'/'.$this->css_path.'/'.$path.'.css');
 		}
 	}
 
 	//* Adiciona um ficheiro de js ao header
 	protected function setJsFiles($array)
 	{
-		foreach($array as $key => $path){
-			$this->data_header['js'][$key] = base_url($this->assets_path.'/'.$this->js_path.'/'.$path.'.js');
+		if(!$array || !is_array($array))
+			return;
+
+		foreach($array as $path){
+			$this->data_header['js'][] = base_url($this->assets_path.'/'.$this->js_path.'/'.$path.'.js');
 		}
 	}
 
@@ -92,7 +100,7 @@ abstract class my_controller extends CI_Controller {
 	//* Funcionalidade que define alguma data qualquer da pagina
 	protected function setData($array)
 	{
-		if(!$array)
+		if(!$array || !is_array($array))
 			return;
 
 		foreach($array as $key => $value){
@@ -103,7 +111,7 @@ abstract class my_controller extends CI_Controller {
 	//* Funcionalidade que define alguma data qualquer do footer
 	protected function setFooterData($array)
 	{
-		if(!$array)
+		if(!$array || !is_array($array))
 			return;
 
 		foreach($array as $key => $value){
@@ -114,7 +122,7 @@ abstract class my_controller extends CI_Controller {
 	//* Funcionalidade que define alguma data qualquer do header
 	protected function setHeaderData($array)
 	{
-		if(!$array)
+		if(!$array || !is_array($array))
 			return;
 
 		foreach($array as $key => $value){
@@ -122,33 +130,45 @@ abstract class my_controller extends CI_Controller {
 		}
 	}
 
-	//* Funcionalidade que verifica se todas as datas necessárias foram preenchidas no header e no footer
-	//* e se não foram preenche com undefined
-	private function verify_datas()
+	//* Funcionalidade que define alguma data qualquer do menu
+	protected function setNavData($array)
 	{
-		for($i = 0; $i < count($this->data_needed_header); $i++){
-			$key = $this->data_needed_header[$i];
-			if(!isset($this->data_header[$key])){
-				$this->data_header[$key] = 'Undefined';
-			}
+		if(!$array || !is_array($array))
+			return;
+
+		foreach($array as $key => $value){
+			$this->data_nav[$key] = $value;
 		}
-		for($i = 0; $i < count($this->data_needed_footer); $i++){
-			$key = $this->data_needed_footer[$i];
-			if(!isset($this->data_footer[$key])){
-				$this->data_footer[$key] = 'Undefined';
-			}
+	}
+
+	//* Definir um novo link
+	protected function setLinkData($array)
+	{
+		if(!$array || !is_array($array))
+			return;
+
+		foreach($array as $key => $path){
+			$this->data_body['link'][$key] = base_url($path);
 		}
 	}
 
 	//! Define o ficheiro de css main do site
 	private function setMainCssFile()
 	{
-		$this->data_header['css']['main'] = base_url($this->assets_path.'/'.$this->css_path.'/'.$this->main_css_path.'.css');
+		$this->data_header['css'][] = base_url($this->assets_path.'/'.$this->css_path.'/'.$this->main_css_path.'.css');
 	}
 
 	//! Define o css do nav
 	private function setNavCssFile()
 	{
-		$this->data_header['css']['menu'] = base_url($this->assets_path.'/'.$this->css_path.'/'.$this->nav_css_path.'.css');
+		$this->data_header['css'][] = base_url($this->assets_path.'/'.$this->css_path.'/'.$this->nav_css_path.'.css');
+	}
+
+	//! Cria os arrays multidimensionais
+	private function create_data_arrays()
+	{
+		$this->data_header['css'] = array();
+		$this->data_header['js'] = array();
+		$this->data_header['link'] = array();
 	}
 }
